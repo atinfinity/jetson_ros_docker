6c5933359074a9efdea980380df4548562116110
atinfinity <dandelion1124@gmail.com>
2020-02-16 20:44:31 +0900

    +launch_container.sh

---
+++ launch_container.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run --runtime=nvidia --rm -it \
+           --volume=$XSOCK:$XSOCK:rw \
+           --volume=$XAUTH:$XAUTH:rw \
+           --volume=$HOME:$HOME \
+           --env="XAUTHORITY=${XAUTH}" \
+           --env="DISPLAY=${DISPLAY}" \
+           --env=TERM=xterm-256color \
+           --env=QT_X11_NO_MITSHM=1 \
+           --net=host \
+           jetson/ros:melodic-ngc \
+           bash
